c6fbc71ded118c45209b25bd001a2408d5f2f045
WH1NER7 <danil-2208@mail.ru>
2023-10-12 18:11:24 +0500

    added image to competiton msgs

--- bot/keyboards/reply.py
+++ bot/keyboards/reply.py
@@ -3,5 +3,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 start_kb_markup = ReplyKeyboardMarkup(resize_keyboard=True)
 start_kb_button1 = KeyboardButton("Служба заботы")
-start_kb_button2 = KeyboardButton("Конкурс")
-start_kb_markup.add(start_kb_button1, start_kb_button2)
+# start_kb_button2 = KeyboardButton("Конкурс")
+start_kb_button3 = KeyboardButton("Наш магазин")
+start_kb_markup.add(start_kb_button1, start_kb_button3)
