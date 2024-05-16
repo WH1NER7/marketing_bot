51c0a39341bcd05e938f0eae4040e6f6b9bd230a
WH1NER7 <danil-2208@mail.ru>
2024-05-16 18:59:48 +0500

    added buttons clicks counter

--- bot/database/methods/insert.py
+++ bot/database/methods/insert.py
@@ -18,7 +18,7 @@ def create_user(user_name, user_second_name, user_id, username):
 
 def insert_broadcast_stats(banned, users_get):
     # Получаем текущую дату и время
-    current_date = datetime.now().strftime('%d-%m-%Y')
+    current_date = datetime.now().strftime('%d.%m.%Y')
 
     # Создаем документ для вставки в коллекцию
     broadcast_stats = {
