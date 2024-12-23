6787385dfab7be08763d0f749010a55d36282c2f
WH1NER7 <danil-2208@mail.ru>
2024-12-23 12:04:06 +0500

    added buttons clicks counter

--- bot/handlers/user/main.py
+++ bot/handlers/user/main.py
@@ -200,10 +200,10 @@ async def get_link(message: Message, state: FSMContext):
 async def our_shop_link(message: Message):
     increment_button_counter("our_shop_link")
 
-    # photo = types.InputFile('bot/images/wb_ozon.png')
-    text_with_link = "Бонусы"
+    photo = types.InputFile('bot/images/wb_ozon.png')
+    text_with_link = "Благодаря размещению на онлайн-площадках с собственной логистикой мы можем предлагать самые приятные цены и доставлять товар в кратчайшие сроки"
 
-    await message.answer(text=text_with_link, parse_mode=types.ParseMode.MARKDOWN)
+    await message.answer_photo(caption=text_with_link, photo=photo, parse_mode=types.ParseMode.MARKDOWN, reply_markup=shop_kb)
 
 
 async def ready_present(message: types.Message):
