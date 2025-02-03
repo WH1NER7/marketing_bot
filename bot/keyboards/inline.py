7afb7866d4f1d2ddbd2c00d9654370219c652e6d
WH1NER7 <danil-2208@mail.ru>
2025-02-03 19:50:26 +0500

    added buttons clicks counter

--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -73,6 +73,6 @@ problems_kb_add.add(problems_kb_btn1)
 
 
 advert_kb = InlineKeyboardMarkup(row_width=1)
-advert_button1 = InlineKeyboardButton(text="Перейти", url="https://t.me/mykbrend")
+advert_button1 = InlineKeyboardButton(text="Посмотреть наши комплекты", url="https://www.wildberries.ru/catalog/0/search.aspx?search=missyourkiss")
 # problems_kb_btn2 = InlineKeyboardButton(text="Получить гайд", url="https://b24-dvx7b9.bitrix24.site/crm_form_ru7u6/")
 advert_kb.add(advert_button1)
